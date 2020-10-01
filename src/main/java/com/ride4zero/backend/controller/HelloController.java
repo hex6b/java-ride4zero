17f3f6951f011174bb7e1adc554e36d5c5216b85
kaigoman <kaigo@protonmail.com>
2020-10-01 17:17:32 +0100

    initial commit

---
+++ src/main/java/com/ride4zero/backend/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.ride4zero.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String getHello() {
+        return "hello from backend";
+    }
+}
---
+++ src/test/java/com/ride4zero/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ride4zero.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
